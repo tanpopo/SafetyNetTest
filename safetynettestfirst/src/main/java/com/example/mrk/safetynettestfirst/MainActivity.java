1596f5cd56e4f24339c9ee9abe490aed53f38c63
mrk <mrk.dev99@gmail.com>
2016-01-09 07:42:19 +0900

    add nonce generation and attest

--- safetynettestfirst/src/main/java/com/example/mrk/safetynettestfirst/MainActivity.java
+++ safetynettestfirst/src/main/java/com/example/mrk/safetynettestfirst/MainActivity.java
@@ -1,19 +1,43 @@
 package com.example.mrk.safetynettestfirst;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.safetynet.SafetyNetApi;
+import com.google.android.gms.common.api.ResultCallback;
+
+public class MainActivity extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private static String tag = "MainActivity";
+
+    private GoogleApiClient mGoogleApiClient = null;
+    private SecureRandom secureRandom = null;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        try {
+            secureRandom = SecureRandom.getInstance("SHA1PRNG");
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(tag, "securerandom initialize failed!");
+        }
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,19 +61,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //attest
-        Button attestButton = (Button)findViewById(R.id.attestButton);
+        Button attestButton = (Button) findViewById(R.id.attestButton);
         attestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(tag, "attest button clicked!");
 
-
-
-                TextView prefValResult = (TextView)findViewById(R.id.outputView);
-                prefValResult.setText("attest result!");
+                attest();
             }
         });
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -69,10 +98,115 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //    protected synchronized void buildGoogleApiClient() {
-//        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                .addApi(SafetyNet.API)
-//                .addConnectionCallbacks(this)
-//                .build();
-//    }
+    private int attest() {
+
+        byte[] nonce = getRequestNonce(); // Should be at least 16 bytes in length.
+        if (mGoogleApiClient == null) {
+            buildGoogleApiClient();
+        }
+        if (nonce == null || mGoogleApiClient == null) {
+            Log.e(tag, "Error: nonce or client is null");
+            return 1;
+        }
+
+//        SafetyNet.SafetyNetApi.attest(mGoogleApiClient, nonce)
+//                .setResultCallback(new ResultCallback<SafetyNetApi.AttestationResult>() {
+//
+//                    @Override
+//                    public void onResult(SafetyNetApi.AttestationResult result) {
+//                        Status status = result.getStatus();
+//                        if (status.isSuccess()) {
+//                            // Indicates communication with the service was successful.
+//                            // result.getJwsResult() contains the result data
+//                            Log.d(tag, "attest success!");
+//
+//                            final String jwsResult = result.getJwsResult();
+//                            if (!TextUtils.isEmpty(jwsResult)) {
+//                                TextView outputView = (TextView) findViewById(R.id.outputView);
+//                                outputView.setText(jwsResult);
+//                            }
+//                        } else {
+//                            // An error occurred while communicating with the service
+//                            Log.e(tag, "attest failed!");
+//                        }
+//                    }
+//                });
+        return 0;
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(SafetyNet.API)
+                .addConnectionCallbacks(this)
+                .build();
+    }
+
+    private byte[] getRequestNonce() {
+        byte[] nonce = null;
+
+        if (secureRandom != null) {
+            nonce = new byte[32];
+            secureRandom.nextBytes(nonce);
+        }
+        return nonce;
+    }
+    ///////////////////////////////////////
+    // override
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.d(tag, "onConnected!");
+
+        Toast.makeText(this, "connected to SafetyNet", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.d(tag, "onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.d(tag, "onConnectionFailed");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.mrk.safetynettestfirst/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.mrk.safetynettestfirst/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
